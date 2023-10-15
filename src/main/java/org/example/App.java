2ca23fc8f77fac79623101e1169e8929236105e6
oguz.sudas <sudasoguz@gmail.com>
2023-10-15 22:16:45 +0300

    practice

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,8 @@ package org.example;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class App {
 
@@ -19,7 +21,23 @@ public class App {
 
         listOfAllEmployeesJoinedIn2023(employeeList);
 
-        printNamesOfEmployeesWithNthHighestSalary(employeeList, 3);
+        printMinSalary(employeeList);
+
+        printNamesOfAllEmployeesWithMinSalary(employeeList);
+
+        listOfAllEmployeesWorkingOnMoreThanTwoProject(employeeList);
+
+        countOfAllLaptopsAssignedToEmployees(employeeList);
+
+        countOfAllProjectsWithGivenNameAsPM(employeeList, "Robert Downey Jr");
+
+        listOfAllProjectsWithGivenNameAsPM(employeeList, "Robert Downey Jr");
+
+        listOfAllPeopleWorkingWithGivenName(employeeList, "Robert Downey Jr");
+
+        createMapOfData(employeeList);
+
+        createMapOfCountJoinedParticularYear(employeeList);
 
     }
 
@@ -69,6 +87,97 @@ public class App {
                 .filter(e -> e.getSalary() == nThHighestEmployee.getSalary())
                 .forEach(k -> System.out.println(k.toString()))
             );
+    }
+
+    public static void printMinSalary(List<Employee> employeeList) {
+        System.out.println("--- 6. Print minimum salary. ---");
+
+        employeeList.stream()
+            .sorted(Comparator.comparing(Employee::getSalary))
+            .findFirst()
+            .ifPresent(System.out::println);
+    }
+
+    public static void printNamesOfAllEmployeesWithMinSalary(List<Employee> employeeList) {
+        System.out.println("--- 7. Print list of all employee with min salary. ---");
+
+        employeeList.stream()
+            .sorted(Comparator.comparing(Employee::getSalary))
+            .findFirst()
+            .ifPresent(employee -> employeeList.stream().filter(e -> employee.getSalary() == e.getSalary())
+                .forEach(e -> System.out.println(e.getFirstName() + " " + e.getLastName())));
+    }
+
+    public static void listOfAllEmployeesWorkingOnMoreThanTwoProject(List<Employee> employeeList) {
+        System.out.println("--- 8. List of people working on more than 2 projects. ---");
+
+        employeeList.stream()
+            .filter(employee -> employee.getProjects().size() >= 2)
+            .forEach(e -> System.out.println(e.getFirstName() + " " + e.getLastName()));
+    }
+
+    public static void countOfAllLaptopsAssignedToEmployees(List<Employee> employeeList) {
+        System.out.println("--- 9. Count of total laptops assigned to the employees. ---");
+
+        int sumOfLaptos = employeeList.stream()
+            .mapToInt(Employee::getTotalLaptopsAssigned)
+            .sum();
+        System.out.println("Sum of total laptops : " + sumOfLaptos);
+    }
+
+    public static void countOfAllProjectsWithGivenNameAsPM(List<Employee> employeeList, String name) {
+        System.out.println("--- 10. Count of all projects with Robert Downey Jr as PM. ---");
+
+        var count = employeeList.stream()
+            .flatMap(employee -> employee.getProjects().stream())
+            .filter(project -> name.equalsIgnoreCase(project.getProjectManager()))
+            .distinct()
+            .count();
+
+        System.out.println("Count of all projects with Robert Downey Jr as PM : " + count);
+    }
+
+    public static void listOfAllProjectsWithGivenNameAsPM(List<Employee> employeeList, String name) {
+        System.out.println("--- 11. List of all projects with Robert Downey Jr as PM. ---");
+
+        employeeList.stream()
+            .flatMap(employee -> employee.getProjects().stream())
+            .filter(project -> name.equalsIgnoreCase(project.getProjectManager()))
+            .distinct()
+            .forEach(project -> System.out.println(project.getName()));
+    }
+
+    public static void listOfAllPeopleWorkingWithGivenName(List<Employee> employeeList, String name) {
+        System.out.println("--- 12. List of all people working with Robert Downey Jr. ---");
+
+        employeeList.stream()
+            .filter(employee -> employee.getProjects().stream().anyMatch(p -> name.equalsIgnoreCase(p.getProjectManager())))
+            .distinct()
+            .forEach(e -> System.out.println(e.getFirstName() + " " + e.getLastName()));
+    }
+
+    public static void createMapOfData(List<Employee> employeeList) {
+        System.out.println("--- 13. Create a map based on this data, they key should be the year of joining, and value should be list of all the employees who joined the particular year. ---");
+
+        Map<String, List<Employee>> mapOfData = employeeList.stream()
+            .collect(Collectors.groupingBy(s -> s.getId().substring(0, 4), Collectors.toList()));
+
+        mapOfData.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey())
+            .forEachOrdered(entry -> {
+                System.out.println("Year : " + entry.getKey());
+                entry.getValue().forEach(employee -> System.out.println(" " + employee.getFirstName() + " " + employee.getLastName()));
+            });
+    }
+
+    private static void createMapOfCountJoinedParticularYear(List<Employee> employeeList) {
+        System.out.println("--- 14. Create a map based on this data, the key should be year of joining and value should be the count ---");
+        var createData = employeeList.stream()
+            .collect(Collectors.groupingBy(employee -> employee.getId().substring(0, 4), Collectors.counting()));
+
+        createData.entrySet().stream()
+            .sorted(Map.Entry.comparingByKey())
+            .forEachOrdered(entry -> System.out.println("Year: " + entry.getKey() + " count:" + entry.getValue()));
     }
 
 }
